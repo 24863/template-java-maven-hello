eb58d3a1992d8d49603472dd0f65f6dd3b6f051e
24863 <153067817+24863@users.noreply.github.com>
2024-03-29 10:48:46 +0800

    Add files via upload

---
+++ src/main/java/org/example/Circle.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Circle implements Shape {
+ 
+    @Override
+    public void draw() {
+       System.out.println("Hello world!Inside Circle::draw() method.");
+    }
+ }
---
+++ src/main/java/org/example/Square.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Square implements Shape {
+ 
+    @Override
+    public void draw() {
+       System.out.println("Hello world!Inside Square::draw() method.");
+    }
+ }
